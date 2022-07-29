4c98ab54446c1d2199a1a010239de0932de723e6
Bkc4444 <bkc4444@gmail.com>
2022-07-28 20:03:20 -0700

    Created Supplements class

---
+++ ResidentEvilInventoryApp/Classes/Supplements.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResidentEvilInventoryApp.Classes
+{
+    internal class Supplements
+    {
+        // this will be a combining tool
+       string Supplement { get; set; }
+    }
+}
