9caec22fdf6e9523b8fceef0d7410df6d18dfc5d
Bkc4444 <bkc4444@gmail.com>
2022-07-28 19:47:58 -0700

    Created Weapons class

--- ResidentEvilInventoryApp/Classes/Backpack.cs
+++ ResidentEvilInventoryApp/Classes/Backpack.cs
@@ -5,17 +5,17 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace ResidentEvilInventoryApp
+namespace ResidentEvilInventoryApp.Classes
 {
     public class Backpack
     {
         // this is set to a certain amount
         // if under or over certain amount
         // say Justin Lim is dissapointed
-        public int Inventory { get; set; } 
+        public int Inventory { get; set; }
 
 
-        public int SlotsUsed { get; set; } 
+        public int SlotsUsed { get; set; }
 
     }
 }
