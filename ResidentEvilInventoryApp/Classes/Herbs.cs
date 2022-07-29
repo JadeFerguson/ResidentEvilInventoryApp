28c7061cb53cc0ee1ca592c4f6f0cfa0e8d54f2a
Bkc4444 <bkc4444@gmail.com>
2022-07-28 20:03:04 -0700

    Created Herbs class

---
+++ ResidentEvilInventoryApp/Classes/Herbs.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResidentEvilInventoryApp.Classes
+{
+    internal class Herbs
+    {
+        string GreenHerb { get; set; }
+
+        string BlueHerb { get; set; }
+
+        string RedHerb { get; set; }
+
+        string YellowHerb { get; set; }
+    }
+}
