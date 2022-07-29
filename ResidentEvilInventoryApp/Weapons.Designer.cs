d2f12092f3a57beb19878e251025473dfbf7c5c5
Jade Ferguson <jadesnider2440@gmail.com>
2022-07-28 19:36:31 -0700

    Made initial windows forms for each category

--- ResidentEvilInventoryApp/Weapons.Designer.cs
+++ ResidentEvilInventoryApp/Weapons.Designer.cs
@@ -28,12 +28,56 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.lstWeapons = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(414, 76);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(322, 114);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(414, 259);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(322, 117);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // lstWeapons
+            // 
+            this.lstWeapons.FormattingEnabled = true;
+            this.lstWeapons.ItemHeight = 37;
+            this.lstWeapons.Location = new System.Drawing.Point(44, 76);
+            this.lstWeapons.Name = "lstWeapons";
+            this.lstWeapons.Size = new System.Drawing.Size(297, 300);
+            this.lstWeapons.TabIndex = 2;
+            // 
+            // Weapons
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 37F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstWeapons);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Weapons";
             this.Text = "Weapons";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Button btnAdd;
+        private Button btnDelete;
+        private ListBox lstWeapons;
     }
 }
