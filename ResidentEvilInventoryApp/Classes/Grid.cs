41f3d678c911ec4504d8317c05d5a10391c6b15f
Bkc4444 <bkc4444@gmail.com>
2022-08-02 19:26:00 -0700

    Created a Grid class for the inventory

---
+++ ResidentEvilInventoryApp/Classes/Grid.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResidentEvilInventoryApp.Classes
+{
+    internal class Grid
+    {
+        private int width;
+        private int height;
+        private int[,] gridArray;
+
+        public Grid(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+
+            gridArray = new int[width, height];
+
+            Debug.Write(width + " " + height);
+        }
+    }
+}
