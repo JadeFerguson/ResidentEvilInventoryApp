ed41dcb181985676cd7f179f7c15137e5c60bac8
Jade Ferguson <jadesnider2440@gmail.com>
2022-08-11 19:37:31 -0700

    Added List

--- ResidentEvilInventoryApp/Forms/Fuel.cs
+++ ResidentEvilInventoryApp/Forms/Fuel.cs
@@ -16,5 +16,29 @@ namespace ResidentEvilInventoryApp
         {
             InitializeComponent();
         }
+
+        private void PopulateFuelListBox()
+        {
+            lstFuel.Items.Add("Fuel");
+            lstFuel.Items.Add("Molitovs");
+        }
+
+        private void lstFuel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void frmFuel_Load(object sender, EventArgs e)
+        {
+            PopulateFuelListBox();
+        }
+
+        private void btnAddFuel_Click(object sender, EventArgs e)
+        {
+            InventoryContext dbContext = new();
+
+            string chosenItem = (string)lstFuel.SelectedItem;
+            dbContext.Add(chosenItem);
+        }
     }
 }
