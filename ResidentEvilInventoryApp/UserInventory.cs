36bc10112a1418d96fc4e19bed6c6f0a812942fa
Jade Ferguson <jadesnider2440@gmail.com>
2022-08-17 13:53:06 -0700

    Fixed UserInventory class so we can populate a list to delete from, for delete

--- ResidentEvilInventoryApp/UserInventory.cs
+++ ResidentEvilInventoryApp/UserInventory.cs
@@ -5,13 +5,20 @@ namespace ResidentEvilInventoryApp
 {
     public partial class UserInventory
     {
+        // ? needed after string so its optional and does not add null to every column
         public byte UserId { get; set; }
-        public string Ammo { get; set; } = null;
-        public string FirstAid { get; set; } = null;
-        public string Fuel { get; set; } = null;
-        public string Herbs { get; set; } = null;
-        public string Misc { get; set; } = null;
-        public string Supplements { get; set; } = null;
-        public string Weapons { get; set; } = null;
+        public string? Ammo { get; set; } = null;
+        public string? FirstAid { get; set; } = null;
+        public string? Fuel { get; set; } = null;
+        public string? Herbs { get; set; } = null;
+        public string? Misc { get; set; } = null;
+        public string? Supplements { get; set; } = null;
+        public string? Weapons { get; set; } = null;
+
+        // Needed to ovveride the null needed and to list the items in a list book to delete
+        public override string ToString()
+        {
+            return Ammo + FirstAid + Fuel + Herbs + Misc + Supplements + Weapons;
+        }
     }
 }
