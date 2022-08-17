a29182e14263afe132e74f77e4e8c1f4023adf95
Jade Ferguson <jadesnider2440@gmail.com>
2022-08-16 17:31:30 -0700

    Changed not null to null in UserInventory to match database

--- ResidentEvilInventoryApp/UserInventory.cs
+++ ResidentEvilInventoryApp/UserInventory.cs
@@ -6,12 +6,12 @@ namespace ResidentEvilInventoryApp
     public partial class UserInventory
     {
         public byte UserId { get; set; }
-        public string Ammo { get; set; } = null!;
-        public string FirstAid { get; set; } = null!;
-        public string Fuel { get; set; } = null!;
-        public string Herbs { get; set; } = null!;
-        public string Misc { get; set; } = null!;
-        public string Supplements { get; set; } = null!;
-        public string Weapons { get; set; } = null!;
+        public string Ammo { get; set; } = null;
+        public string FirstAid { get; set; } = null;
+        public string Fuel { get; set; } = null;
+        public string Herbs { get; set; } = null;
+        public string Misc { get; set; } = null;
+        public string Supplements { get; set; } = null;
+        public string Weapons { get; set; } = null;
     }
 }
