e9e2755c6825de32c9c28f258a8f93edb24b8cad
Brandon Carroll <bkc4444@gmail.com>
2022-08-23 19:15:01 -0700

    Database is now run by EF Core

--- ResidentEvilInventoryApp/Forms/Herbs.cs
+++ ResidentEvilInventoryApp/Forms/Herbs.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ResidentEvilInventoryApp.Classes;
+using ResidentEvilInventoryApp.Data;
 
 namespace ResidentEvilInventoryApp
 {
---
+++ ResidentEvilInventoryApp/Migrations/20220824020001_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ResidentEvilInventoryApp.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserInventory",
+                columns: table => new
+                {
+                    UserId = table.Column<byte>(type: "tinyint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ammo = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    FirstAid = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Fuel = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Herbs = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Misc = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Supplements = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true),
+                    Weapons = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK__UserInve__1788CC4CEA803FC1", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserInventory");
+        }
+    }
+}
